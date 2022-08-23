5fde4de0925c4005d484ff5b1c55ab42087439c1
Florent <109805542+FlorentGargot@users.noreply.github.com>
2022-08-23 03:23:46 +0200

    stylized project tiles

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -11,21 +11,27 @@ export class ProjectService{
         name: 'Mon premier projet',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, hic? Blanditiis eos dignissimos veniam, dolore sint esse consequuntur harum perferendis eum quos. Est sequi cupiditate vitae soluta pariatur quod magnam iure sint hic laboriosam. Perferendis quae atque vitae eum voluptatibus.',
         dateAdded: new Date(),
-        technologies: [1,2,3]
+        technologies: [1,2,3],
+        anchorText: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem accusantium magnam amet beatae fuga animi vel ipsam hic facere doloremque!',
+        imageUrl: './assets/images/code-informatique.jpg'
     },
     {
         id: 2,
         name: 'Mon projet Angular',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, hic? Blanditiis eos dignissimos veniam, dolore sint esse consequuntur harum perferendis eum quos. Est sequi cupiditate vitae soluta pariatur quod magnam iure sint hic laboriosam. Perferendis quae atque vitae eum voluptatibus.',
         dateAdded: new Date(),
-        technologies: [4,5,6]
+        technologies: [4,5,6],
+        anchorText: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem accusantium magnam amet beatae fuga animi vel ipsam hic facere doloremque!',
+        imageUrl: './assets/images/source-code_featured-image.png'
     },    
     {
         id: 3,
         name: 'Mon projet React',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, hic? Blanditiis eos dignissimos veniam, dolore sint esse consequuntur harum perferendis eum quos. Est sequi cupiditate vitae soluta pariatur quod magnam iure sint hic laboriosam. Perferendis quae atque vitae eum voluptatibus.',
         dateAdded: new Date(),
-        technologies: [3,6,7,8]
+        technologies: [3,6,7,8],
+        anchorText: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem accusantium magnam amet beatae fuga animi vel ipsam hic facere doloremque!',
+        imageUrl: './assets/images/Best-Code-and-Text-Editors.png'
     }];
 
     constructor(private technologyService: TechnologyService){
