6d55e292bfb0ce0794437831326a13d159b38e9e
Florent <109805542+FlorentGargot@users.noreply.github.com>
2022-08-25 01:11:07 +0200

    implemented single project server response

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const { db_config } = require('./db_config');
+const {projects, technologies} = require('./data');
 
 let connect = mysql.createConnection({
     host: db_config.db_host,
@@ -22,106 +23,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/projects', (req, res, next) => {
 
-    const projects= [{
-        id: 1,
-        name: 'Mon premier projet',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, hic? Blanditiis eos dignissimos veniam, dolore sint esse consequuntur harum perferendis eum quos. Est sequi cupiditate vitae soluta pariatur quod magnam iure sint hic laboriosam. Perferendis quae atque vitae eum voluptatibus.',
-        dateAdded: new Date(),
-        technologies: [1,2,3],
-        anchorText: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem accusantium magnam amet beatae fuga animi vel ipsam hic facere doloremque!',
-        imageUrl: './assets/images/code-informatique.jpg'
-    },
-    {
-        id: 2,
-        name: 'Mon projet Angular',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, hic? Blanditiis eos dignissimos veniam, dolore sint esse consequuntur harum perferendis eum quos. Est sequi cupiditate vitae soluta pariatur quod magnam iure sint hic laboriosam. Perferendis quae atque vitae eum voluptatibus.',
-        dateAdded: new Date(),
-        technologies: [4,5,6],
-        anchorText: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem accusantium magnam amet beatae fuga animi vel ipsam hic facere doloremque!',
-        imageUrl: './assets/images/source-code_featured-image.png'
-    },    
-    {
-        id: 3,
-        name: 'Mon projet React',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, hic? Blanditiis eos dignissimos veniam, dolore sint esse consequuntur harum perferendis eum quos. Est sequi cupiditate vitae soluta pariatur quod magnam iure sint hic laboriosam. Perferendis quae atque vitae eum voluptatibus.',
-        dateAdded: new Date(),
-        technologies: [3,6,7,8],
-        anchorText: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem accusantium magnam amet beatae fuga animi vel ipsam hic facere doloremque!',
-        imageUrl: './assets/images/Best-Code-and-Text-Editors.png'
-    }];
+app.use('/api/projects/:id', (req, res, next) => {
+    const id = +req.params.id;
+    const project = projects.find(el => el.id === id);
+    if(project !== undefined) res.status(200).json(project);
+    else res.status(404).json('Resource cannot be found');
+
+});
+
+app.use('/api/projects', (req, res, next) => {
 
     res.status(200).json(projects);
 });
 
 app.use('/api/technologies', (req, res, next) => {
-    const technologies = [
-        {
-            id: 1,
-            name: 'HTML',
-            description: '',
-            state: 4,
-            color: "#0000c0",
-            level: 4
-        },        
-        {
-            id: 2,
-            name: 'CSS',
-            description: '',
-            state: 4,
-            color: "#0080c0",
-            level: 4
-        },        
-        {
-            id: 3,
-            name: 'JavaScript',
-            description: '',
-            state: 4,
-            color: "#008040",
-            level: 3
-        },        
-        {
-            id: 4,
-            name: 'TypeScript',
-            description: '',
-            state: 4,
-            color: "#200080",
-            level: 4
-        },        
-        {
-            id: 5,
-            name: 'Angular',
-            description: '',
-            state: 4,
-            color: "#c00040",
-            level: 4
-        },        
-        {
-            id: 6,
-            name: 'SCSS',
-            description: '',
-            state: 4,
-            color: "#c08040",
-            level: 4
-        },        
-        {
-            id: 7,
-            name: 'React',
-            description: '',
-            state: 2,
-            color: "#0000e0",
-            level: 4
-        },        
-        {
-            id: 8,
-            name: 'JSX',
-            description: '',
-            state: 2,
-            color: "#40c040",
-            level: 4
-        }
-    ];
+
 
     res.status(200).json(technologies);
 })
